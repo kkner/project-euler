a69c137f8ebcb78e27d78c42d21a8fa2e94df63a
Kaan Kaner <kaankaner@gmail.com>
2023-02-26 19:55:06 +0300

    added FacTest

--- project_euler/src/kknr/euler/AllTests.java
+++ project_euler/src/kknr/euler/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 import kknr.euler.prime.PrimesTest;
+import kknr.euler.util.FacTest;
 import kknr.euler.util.FibBTest;
 import kknr.euler.util.FibTest;
 import kknr.euler.util.UtilTest;
@@ -14,6 +15,7 @@ import kknr.euler.util.UtilTest;
 	UtilTest.class,
 	FibTest.class,
 	FibBTest.class,
+	FacTest.class,
 	PrimesTest.class,
 	})
 public class AllTests {
