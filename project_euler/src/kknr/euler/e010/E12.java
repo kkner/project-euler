ef0dcdb92f15bcdd4ffdedbc85929e922c5cf1eb
Kaan Kaner <kaankaner@gmail.com>
2023-02-26 14:00:35 +0300

    comments & refactor

--- project_euler/src/kknr/euler/e010/E12.java
+++ project_euler/src/kknr/euler/e010/E12.java
@@ -3,26 +3,6 @@ package kknr.euler.e010;
 import kknr.euler.Solution;
 import kknr.euler.util.Util;
 
-/**
- * 
- * Number of divisors = powers of every distinct prime factor + 1 multiplied.
- * 
- * 9800 = 2^3 * 5^2 * 7^2
- * has 4 * 3 * 3 = 36 divisors.
- * 
- * A triangular number:
- * T(n) = (n + 1) * n / 2
- * 
- * n and (n + 1) are coprimes.  
- * There are no shared prime factors.
- *  
- * Simply multiply the number of divisors of the two terms. 
- * 
- * divisors(T(n)) = divisors(n / 2) * divisors(n + 1)
- * 
- * 
- * 
- */
 public class E12 extends Solution
 {
 	public E12(boolean doPrint) {
@@ -31,6 +11,23 @@ public class E12 extends Solution
 
 	public long solve()
 	{
+		/*
+		 * Number of divisors = powers of every distinct prime factor + 1 multiplied.
+		 * 
+		 * 9800 = 2^3 * 5^2 * 7^2
+		 * has 4 * 3 * 3 = 36 divisors.
+		 * 
+		 * A triangular number:
+		 * T(n) = (n + 1) * n / 2
+		 * 
+		 * n and (n + 1) are coprimes.  
+		 * There are no shared prime factors.
+		 *  
+		 * Simply multiply the number of divisors of the two terms. 
+		 * 
+		 * divisors(T(n)) = divisors(n / 2) * divisors(n + 1)
+		 */
+		
 		// Divisor count from (n-1).
 		int prev = 1;
 		
@@ -60,50 +57,68 @@ public class E12 extends Solution
 		}		
 	}
 
+	/**
+	 * Counts the number of all divisors for a given number
+	 * (including 1 and the number itself).
+	 * 
+	 * This method evaluates 2 and all odd numbers as
+	 * prime factor candidates, by repeatedly trying to divide x.
+	 * If a candidate can no longer divide x, the next candidate 
+	 * is selected. 
+	 * 
+	 * At any moment, what remains of x has no divisors < candidate. 
+	 * If candidate * d = x and d < candidate, thi means that x has 
+	 * no remaining divisors other than itself. This means we can stop 
+	 * when candidate > sqrt(x). 
+	 * 
+	 * @param x The number to count divisors of.
+	 * @return Number of divisors calculated.
+	 */
 	public static int countDivisors(int x)
 	{
-		// Whenever p changes, we will multiply prod with pCount.
+		// We will multiply prod with count whenever cand is updated.
 		int prod = 1;
 	
 		// Current prime factor candidate.
-		int p = 2;
+		int cand = 2;
 		
-		// Number of occurrences of p found.
-		int pCount = 1;
+		// 1 + Number of times candidate divided x.
+		int count = 1;
 		
 		while(x > 1) // While not depleted,
 		{	
-			int d = x / p; // Try to divide.			
+			int d = x / cand; // Try to divide.			
 			
-			if (d * p == x) // If it divides, 
+			if (d * cand == x) // If it divides, 
 			{
 				// Found a prime factor.
-				pCount++; // Increment found counter.
+				count++; // Increment found counter.
 				x = d;    // Update remaining.
 			}
-			else 
+			else // If it couldn't divide,
 			{				
-				prod *= pCount; // pCount instances of p
-				pCount = 1;     // Reset counter.
-				// It still works when no occurences of p 
-				// were found because pCount starts from 1.
+				prod *= count; // Occurences of cand + 1.   
+				count = 1;     // Reset the counter.
 				
-				if (d < p) // If p >= sqrt(current x), 
+				// It still works when there were no occurences 
+				// of p because count starts from 1.
+				
+				if (d < cand) // cand >= sqrt(current x), 
 				{
 					// No more prime factors are possible.
 					// Current value of x is the last prime factor.
-					p = x;
+					cand = x;
 				}
-				else //  p < sqrt(current x)
+				else // cand < sqrt(current x)
 				{
 					// Try the next number in the sequence: 2, 3, 5, 7, 9...
-					p += (p & 1) + 1; 
+					cand += (cand & 1) + 1; 
 				}				
 			}
 		}
 		
-		// There can be one more unique prime factor.
-		prod *= pCount;
+		// The last candidate wasn't evaluated.
+		prod *= count;
 		
 		return prod;
 	}
