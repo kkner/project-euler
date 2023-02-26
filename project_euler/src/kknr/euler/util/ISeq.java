386d1bf5d3f8362f5c3390892cc9829eda2723f6
Kaan Kaner <kaankaner@gmail.com>
2023-02-26 19:53:37 +0300

    added comments

--- project_euler/src/kknr/euler/util/ISeq.java
+++ project_euler/src/kknr/euler/util/ISeq.java
@@ -2,7 +2,22 @@ package kknr.euler.util;
 
 public interface ISeq<T> 
 {
-	T curr();
+	/**
+	 * Can adv be called once more?
+	 * @return
+	 */
 	boolean has();
+	
+	/**
+	 * Find and return the next item in the sequence. 
+	 * @return
+	 */
 	T adv();
+	
+	/**
+	 * Returns the last found item.
+	 * adv must be called at least once before this.
+	 * @return
+	 */
+	T curr();
 }
